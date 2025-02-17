830aa073b53070554cd75a38c68b5bb55e933cda
WMike <miguelwcaamaya0911@gmail.com>
2025-02-16 21:48:53 -0500

    add tailwind-css styles global and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,12 @@ interface Props {
 
 export function Header({ onAddTodo }: Props) {
   return (
-    <header>
-      <h1 className="title">Todo</h1>
+    <header className="flex flex-col items-center">
+      <div className="flex w-full m-4">
+        <h1 className="mx-2 font-bold p-1 w-2/10 text-2xl bg-[#262A2F] text-center rounded-xl">
+          o-<span className="text-[#1B5BE4]">o</span>
+        </h1>
+      </div>
       <CreateTodo saveTodo={onAddTodo} />
     </header>
   );
